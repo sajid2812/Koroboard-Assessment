e74fff696c608a04cd14dc83964cb03138887758
Sk Sajid <ssk73573@gmail.com>
2022-08-20 10:23:14 +0530

    complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { dataArray } from 'src/assets/card-data';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  cards = dataArray;
   title = 'koroboard';
 }
---
+++ src/assets/card-data.ts
@@ -0,0 +1,32 @@
+export const dataArray = [
+  {
+    number: 23,
+    info: 'Milestones',
+    link: 'View All Milestones',
+  },
+  {
+    number: 175,
+    info: 'Task Tracked',
+    link: 'View All Milestones',
+  },
+  {
+    number: 6,
+    info: 'Team Members',
+    link: 'View All Members',
+  },
+  {
+    number: 1.5,
+    info: 'GB Data Uploaded',
+    link: 'View All Files',
+  },
+  {
+    number: 73,
+    info: 'Meetings Taken',
+    link: 'View All Meetings',
+  },
+  {
+    number: 162,
+    info: 'Action Items',
+    link: 'View All Meetings',
+  },
+];
